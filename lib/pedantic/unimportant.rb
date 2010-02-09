687900ef2a8f5ada3b39c0bad0c9066fdd8551cb
Pat Allan <pat@freelancing-gods.com>
2010-02-09 16:26:16 +1100

    Adding a missing space (as pointed out by Ben Hoskings).

--- lib/pedantic/unimportant.rb
+++ lib/pedantic/unimportant.rb
@@ -6,7 +6,7 @@ module Pedantic::Unimportant
   def remove_unimportant_words(string)
     %w(
       a again all along also an and are as at but by came can cant couldnt did 
-      didn didnt do doesntdont ever first from have her here him how i if in 
+      didn didnt do doesnt dont ever first from have her here him how i if in 
       into is isnt it itll just last least like most my new no not now of on or
       should sinc so some th than this that the their then those to told too
       true try until url us were when whether while with within yes you youll
